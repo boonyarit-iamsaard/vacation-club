7ff2a777fa86eee1b8a1dbe0c102dce609c5f8f8
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-06-02 16:54:06 +0700

    feat(auth): add login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '~/app/(auth)/login/_components/login-form';
+
+export default function LoginPage() {
+  return (
+    <section className="container py-8">
+      <LoginForm />
+    </section>
+  );
+}
