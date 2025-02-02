46a980c503fa6e8035ae1934a48bb03a630be0f2
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-02-02 20:27:19 +0700

    ci: update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
     '**/*': 'prettier --check --ignore-unknown',
     '**/*.js': ['eslint'],
-    '**/*.php': './vendor/bin/duster lint',
+    '**/*.php,!.phpstorm.meta.php,!_ide_helper.php,!_ide_helper_models.php': './vendor/bin/duster lint',
 };
